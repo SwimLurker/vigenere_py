c071d6a1bc9e22ecf6ae2431b81c1694e8b33977
Chandler Li <swimlurker@gmail.com>
2013-01-10 01:14:04 +0800

    modify source code to pass compile

--- src/vigenere.py
+++ src/vigenere.py
@@ -1,49 +1,48 @@
 def main(args):
-	if len(args) != 3:
-		printUsage
-	elif args[0] == '-e':
-		encrypt(args[1], args[2])
-	elif args[0] == '-d':
-		decrypt(args[1], args[2])
-	else:
-		printUsage
-def printUsage:
-	print """
-	Usage: main -e <srouce string> <cipher> //Encrypt source string with the cipher by vigenere algorithm;
-	       main -d <encrypted string> <cipher> //Decrypt encrypted string with the cipher by vigenere algorithm;
-	"""
+    if len(args) != 4:
+        printUsage()
+    elif args[1] == '-e':
+        print(encrypt(args[2], args[3]))
+    elif args[1] == '-d':
+        print(decrypt(args[2], args[3]))
+    else:
+        printUsage()
+def printUsage():
+    print("""
+    Usage: main -e <srouce string> <cipher> //Encrypt source string with the cipher by vigenere algorithm;
+           main -d <encrypted string> <cipher> //Decrypt encrypted string with the cipher by vigenere algorithm;
+    """)
 def encrypt(srcStr, cipher):
-	result = ''
-	j = 0
-	for i in range(len(srcStr)):
-		c = srcStr.upper()[i]
-		if isUpperLetter(c):
-			result += encryptChar(c, cipher.upper()[j%len(cipher)])
-		else:
-			result += ' '
-		j+=1
-		return result
+    result = ''
+    j = 0
+    for i in range(len(srcStr)):
+        c = srcStr.upper()[i]
+        if isUpperLetter(c):
+            result += encryptChar(c, cipher.upper()[j])
+            j = (j + 1) % len(cipher)
+        else:
+            result += ' '
+    return result
 def encryptChar(sc, cc):
-	chr((((sc - 'A'[0]) + (cc - 'A'[0])) % 26) + 'A'[0])
-	
+    return chr((((ord(sc) - ord('A')) + (ord(cc) - ord('A'))) % 26) + ord('A'[0]))
 def decrypt(encStr, cipher):
-	result = ''
-	j = 0
-	for i in range(len(encStr)):
-		c = encStr.upper()[i]
-		if isUpperLetter(c):
-			result += decryptChar(c, cipher.upper()[j%len(cipher)])
-		else:
-			result += ' '
-		j+=1
-		return result
+    result = ''
+    j = 0
+    for i in range(len(encStr)):
+        c = encStr.upper()[i]
+        if isUpperLetter(c):
+            result += decryptChar(c, cipher.upper()[j])
+            j = (j + 1) % len(cipher)
+        else:
+            result += ' '
+    return result
 def decryptChar(ec, cc):
-	chr((((ec + 26) - cc) % 26) + 'A'[0])
+    return chr((((ord(ec) + 26) - ord(cc)) % 26) + ord('A'))
 def isUpperLetter(c):
-	if c >= 'A'[0] and c<= 'Z'[0]:
-		true
-	else:
-		false
-if __name__ == "__main__"
-	import sys
-	main(sys.argv)
+    if c >= 'A' and c<= 'Z':
+        return True
+    else:
+        return False
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
